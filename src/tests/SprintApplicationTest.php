77836b89fddbfc5de9d08616c2d14ecd826df8c3
Christopher Johnson <christopher.johnson@wikimedia.de>
2014-11-26 12:55:44 +0100

    fix tests
    Change-Id: I38883e697fdd18a060216648a4ac6822ddbd5d32

--- src/tests/SprintApplicationTest.php
+++ src/tests/SprintApplicationTest.php
@@ -1,38 +1,38 @@
 <?php
-final class BurndownApplicationTest extends SprintTestCase {
+final class SprintApplicationTest extends SprintTestCase {
 
   public function testGetName () {
-    $burndown_application = new BurndownApplication;
+    $burndown_application = new SprintApplication;
     $name = $burndown_application->getName();
     $this->assertEquals('Sprint', $name);
   }
 
   public function testBaseURI () {
-    $burndown_application = new BurndownApplication;
+    $burndown_application = new SprintApplication;
     $baseURI = $burndown_application->getBaseURI();
     $this->assertEquals('/sprint/report/', $baseURI);
   }
 
   public function testgetIconName() {
-    $burndown_application = new BurndownApplication;
+    $burndown_application = new SprintApplication;
     $icon_name = $burndown_application->getIconName();
     $this->assertEquals('slowvote', $icon_name);
   }
 
   public function testgetShortDescription() {
-    $burndown_application = new BurndownApplication;
+    $burndown_application = new SprintApplication;
     $description = $burndown_application->getShortDescription();
     $this->assertEquals('Build burndowns', $description);
   }
 
   public function testgetEventListeners() {
-    $burndown_application = new BurndownApplication;
+    $burndown_application = new SprintApplication;
     $eventlistener = $burndown_application->getEventListeners();
     $this->assertInstanceOf('BurndownActionMenuEventListener', $eventlistener[0]);
   }
 
   public function testgetRoutes() {
-    $burndown_application = new BurndownApplication;
+    $burndown_application = new SprintApplication;
     $routes = $burndown_application->getRoutes();
     $assertion = array(
         '/sprint/' => array(
