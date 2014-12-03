aa0d8d6a7f83617541f0405f1b36fadd9d14a5d6
Christopher Johnson <christopher.johnson@wikimedia.de>
2014-12-03 17:46:23 +0100

    remove unused methods from BurndownDataDate
    fix tests
    
    Change-Id: Icc9da90452df6699b5821519bab94561506e4b4e

--- src/tests/BurndownDataDateTest.php
+++ src/tests/BurndownDataDateTest.php
@@ -20,24 +20,4 @@ final class BurndownDataDateTest extends SprintTestCase {
     $total = $date->sumTasksTotal($current, $previous);
     $this->assertEquals(13, $total);
   }
-
-  public function testSumTasksRemaining() {
-    $date = new BurndownDataDate('test date');
-    $previous = id(new BurndownDataDate('monday'));
-    $previous->setTasksRemaining('5');
-    $current = id(new BurndownDataDate('tuesday'));
-    $current->setTasksRemaining('2');
-    $total = $date->sumTasksRemaining($current, $previous);
-    $this->assertEquals(7, $total);
-  }
-
-  public function testSumPointsRemaining() {
-    $date = new BurndownDataDate('test date');
-    $previous = id(new BurndownDataDate('monday'));
-    $previous->setPointsRemaining('5');
-    $current = id(new BurndownDataDate('tuesday'));
-    $current->setPointsRemaining('2');
-    $total = $date->sumPointsRemaining($current, $previous);
-    $this->assertEquals(7, $total);
-  }
 }
