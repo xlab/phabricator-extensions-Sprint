71660cfa1215c37bfbc3d6f9f279387ef9a280cc
Christopher Johnson <christopher.johnson@wikimedia.de>
2015-03-02 12:03:56 +0100

    fixes task table break for long task names
    Uses overflow hide with ellipsis and fixed table width
    
    Bug: T89876
    Change-Id: Iec2c990aedabaa060c2dd016a34ee93b2c70ab0a

--- rsrc/behavior-tasks-table.js
+++ rsrc/behavior-tasks-table.js
@@ -7,12 +7,16 @@ JX.behavior('tasks-table', function (config) {
         $('#tasks-list').DataTable({
             "order": [[ 8, "desc" ]],
             "aoColumnDefs": [
+                { "aTargets": [ 0 ], "sWidth": "50%" },
                 { "bVisible": false, "aTargets": [ 1 ] },
-                { "iDataSort": 1, "aTargets": [ 2 ] },
+                { "iDataSort": 1, "aTargets": [ 2 ], "sWidth": "8%" },
                 { "bVisible": false, "aTargets": [ 3 ] },
-                { "iDataSort": 3, "aTargets": [ 4 ] },
+                { "iDataSort": 3, "aTargets": [ 4 ], "sWidth": "8%" },
+                { "aTargets": [ 5 ], "sWidth": "8%" },
                 { "bVisible": false, "aTargets": [ 6 ] },
-                { "iDataSort": 6, "aTargets": [ 7 ] }
+                { "iDataSort": 6, "aTargets": [ 7 ], "sWidth": "8%" },
+                { "aTargets": [ 8 ], "sWidth": "8%" },
+                { "aTargets": [ 9 ], "sWidth": "10%" }
             ]
         });
     });
