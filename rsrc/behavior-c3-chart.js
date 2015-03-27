6f371a5a799bcbfb3b0dc95da11ba2ebc771b125
root <root@localhost.localdomain>
2015-03-27 17:36:36 +0100

    updates c3 and d3 JS libraries
    adds rotated x-axis labels for burndown chart
    
    Change-Id: Ie20951c89283df8c706affa54ec00d8fc3e6854a

--- rsrc/behavior-c3-chart.js
+++ rsrc/behavior-c3-chart.js
@@ -6,31 +6,34 @@ JX.behavior('c3-chart', function(config) {
 
     var h = JX.$(config.hardpoint);
     var l = c3.generate({
-         bindto: h,
-         data: {
-           columns: [
-               config.totalpoints,
-               config.remainingpoints,
-               config.idealpoints,
-               config.pointstoday
-           ],
-             type: 'line',
-             types: {
-                 'Ideal Points': 'spline',
-                 'Points Closed Today': 'bar'
-             },
-         },
-         axis: {
-            x: {
-                type: 'category',
-                categories: config.timeseries,
-                tick: {
-                    culling: {
-                        max: 18
-                    }
+            bindto: h,
+            data: {
+                columns: [
+                    config.totalpoints,
+                    config.remainingpoints,
+                    config.idealpoints,
+                    config.pointstoday
+                ],
+                type: 'line',
+                types: {
+                    'Ideal Points': 'spline',
+                    'Points Closed Today': 'bar'
+                }
+            },
+            axis: {
+                x: {
+                    type: 'category',
+                    categories: config.timeseries,
+                    tick: {
+                        culling: {
+                            max: 18
+                        },
+                        rotate: 50,
+                        multiline: false
+                    },
+                    height: 60
                 }
             }
-         }
-     });
+        });
 });
 
