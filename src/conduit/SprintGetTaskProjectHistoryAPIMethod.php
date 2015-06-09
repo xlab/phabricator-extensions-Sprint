5f9967f07f02860a49f52275a9c0603134593727
Christopher Johnson <christopher.johnson@wikimedia.de>
2015-06-09 15:04:12 +0200

    adds conduit method to get added and removed projects from tasks
    Bug: T100657
    
    Change-Id: If9c740d28c4ee6e04dc5be8ad737dc69816dc24f

--- src/conduit/SprintGetTaskProjectHistoryAPIMethod.php
+++ src/conduit/SprintGetTaskProjectHistoryAPIMethod.php
@@ -11,12 +11,7 @@ final class SprintGetTaskProjectHistoryConduitAPIMethod extends SprintConduitAPI
   }
 
   public function defineParamTypes() {
-    return array(
-        'name'       => 'required string ("name")',
-        'members'    => 'optional list ([<phid>]) - empty list enter []',
-        'startdate'  => 'required string ("YYYY-MM-DD H:i")',
-        'enddate'    => 'required string ("YYYY-MM-DD H:i")',
-    );
+    return array();
   }
 
   public function defineReturnType() {
@@ -34,59 +29,10 @@ final class SprintGetTaskProjectHistoryConduitAPIMethod extends SprintConduitAPI
         ProjectCreateProjectsCapability::CAPABILITY,
         $user);
 
-    $project = PhabricatorProject::initializeNewProject($user);
-    $type_name = PhabricatorProjectTransaction::TYPE_NAME;
-    $members = $request->getValue('members');
-    $xactions = array();
-
-    $xactions[] = id(new PhabricatorProjectTransaction())
-        ->setTransactionType($type_name)
-        ->setNewValue($request->getValue('name'));
-
-    $xactions[] = id(new PhabricatorProjectTransaction())
-        ->setTransactionType(PhabricatorTransactions::TYPE_EDGE)
-        ->setMetadataValue(
-            'edge:type',
-            PhabricatorProjectProjectHasMemberEdgeType::EDGECONST)
-        ->setNewValue(
-            array(
-                '+' => array_fuse($members),
-            ));
-
-    $xactions[] = id(new PhabricatorProjectTransaction())
-        ->setTransactionType(PhabricatorProjectTransaction::TYPE_ICON)
-        ->setNewValue('fa-calendar');
-
-    $xactions[] = id(new PhabricatorProjectTransaction())
-        ->setTransactionType(PhabricatorProjectTransaction::TYPE_COLOR)
-        ->setNewValue('green');
-
-    $xactions[] = id(new PhabricatorProjectTransaction())
-        ->setTransactionType(PhabricatorTransactions::TYPE_CUSTOMFIELD)
-        ->setMetadataValue('customfield:key', 'isdc:sprint:issprint')
-        ->setOldValue(null)
-        ->setNewValue(1);
-
-    $xactions[] = id(new PhabricatorProjectTransaction())
-        ->setTransactionType(PhabricatorTransactions::TYPE_CUSTOMFIELD)
-        ->setMetadataValue('customfield:key', 'isdc:sprint:startdate')
-        ->setOldValue(null)
-        ->setNewValue(strtotime($request->getValue('startdate')));
-
-    $xactions[] = id(new PhabricatorProjectTransaction())
-        ->setTransactionType(PhabricatorTransactions::TYPE_CUSTOMFIELD)
-        ->setMetadataValue('customfield:key', 'isdc:sprint:enddate')
-        ->setOldValue(0)
-        ->setNewValue(strtotime($request->getValue('enddate')));
-
-    $editor = id(new PhabricatorProjectTransactionEditor())
-        ->setActor($user)
-        ->setContinueOnNoEffect(true)
-        ->setContentSourceFromConduitRequest($request);
-
-    $editor->applyTransactions($project, $xactions);
-
-    return $this->buildProjectInfoDictionary($project);
+    $history = id(new SprintQuery())
+        ->setViewer($user)
+        ->getTaskHistory();
+    return $history;
   }
 
 }
