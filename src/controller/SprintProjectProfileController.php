a9e064ee123a3a11e057d653290033eec70d5df6
Christopher Johnson <christopher.johnson@wikimedia.de>
2015-12-26 14:32:08 +0100

    fixes upstream removal of setObjectURI() https://secure.phabricator.com/D14805 17.12.2015 Bug: T122451
    Change-Id: If9f1e00e7327890ff0dbb42f407c988f21712623

--- src/controller/SprintProjectProfileController.php
+++ src/controller/SprintProjectProfileController.php
@@ -79,8 +79,7 @@ final class SprintProjectProfileController
 
     $view = id(new PhabricatorActionListView())
       ->setUser($viewer)
-      ->setObject($project)
-      ->setObjectURI($request->getRequestURI());
+      ->setObject($project);
 
     $can_edit = PhabricatorPolicyFilter::hasCapability(
       $viewer,
