089e9e5dfcfda15a56a50900f7a2c714b6191652
Christopher Johnson <christopher.johnson@wikimedia.de>
2016-01-08 09:11:42 +0100

    fixes call to undefined method setObjectUri Bug: T122451
    Change-Id: I8705f0b604b90ba4a0d49fffaabf78c3bd5b3a39

--- src/controller/board/SprintBoardColumnDetailController.php
+++ src/controller/board/SprintBoardColumnDetailController.php
@@ -84,7 +84,6 @@ final class SprintBoardColumnDetailController
     $base_uri = '/board/'.$project_id.'/';
 
     $actions = id(new PhabricatorActionListView())
-      ->setObjectURI($this->getApplicationURI($base_uri.'column/'.$id.'/'))
       ->setUser($viewer);
 
     $can_edit = PhabricatorPolicyFilter::hasCapability(
